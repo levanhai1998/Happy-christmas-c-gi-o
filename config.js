2b5730734df5b37d164bddcdaa69bea2a62141cc
levanhai1998 <86667736+levanhai1998@users.noreply.github.com>
2022-12-24 22:42:53 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '💘Cô giáo💘';
+const giftUrl = 'https://www.facebook.com/photo.php?fbid=3399466340295574&set=pb.100006967242079.-2207520000.&type=3';
+const eventName = 'happy christmas';
+const titleCard = 'happy christmas';
+const contentCard = 'Chúc mừng giáng sinh an lành 💘';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = '1.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
